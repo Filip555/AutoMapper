0bd5d1b010a05a8a50a19326724667aafd988365
Pawel <pawel.fil555@gmail.com>
2016-08-15 18:03:59 +0200

    .

---
+++ AutoMappers/Program.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+namespace AutoMappers
+{
+public class OldUser
+{
+    public float wiek { get; set; }
+    public string FirstName { get; set; }
+    public string Surname { get; set; }
+}
+public class Users
+{
+    public int wiek { get; set; }
+    public string FirstName { get; set; }
+    public string Surname { get; set; }
+}
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            OldUser personModel = new OldUser
+            {
+                wiek = 50.5f,
+                FirstName = "Anna",
+                Surname = "Nowak",
+            };
+
+            var config = new MapperConfiguration(cfg =>
+
+            cfg.CreateMap<OldUser, Users>()
+            //cfg.CreateMap<OldUser, Users>().ForMember(x => x.a, s => s.MapFrom(d => d.Surname))
+            //cfg.CreateMap<OldUser,Users>().ForMember(x=>x.FirstName,s=>s.Ignore())
+
+            );
+            config.AssertConfigurationIsValid();
+            var user = config.CreateMapper();
+            Users test = user.Map<OldUser, Users>(personModel);          
+            
+
+        }
+    }
+}
